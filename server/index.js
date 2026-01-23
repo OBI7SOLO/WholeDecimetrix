524ef606034a96288c318174e883f88bf4f2b9f0
OBI7SOLO <andersonlosada.11@gmail.com>
2026-01-23 01:13:23 -0500

    fix: Update route handling for static assets in production to match all paths

--- server/index.js
+++ server/index.js
@@ -176,7 +176,7 @@ app.delete('/users/:id', authenticateJWT, requireAdmin, async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   });
 }
