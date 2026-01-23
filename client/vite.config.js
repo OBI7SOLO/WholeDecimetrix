36c224133cc21d7f05d9da6dc6f9369b8063338c
OBI7SOLO <andersonlosada.11@gmail.com>
2026-01-23 01:17:49 -0500

    refactor: Enhance Vite configuration with build options for chunk management

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('mapbox-gl')) {
+              return 'mapbox';
+            }
+            if (id.includes('@mui')) {
+              return 'mui';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
